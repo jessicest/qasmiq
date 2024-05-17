ca2fbeea712833c1efa5d28d35c212c6a0901505
Jessica Knight <corence@gmail.com>
2024-05-17 14:45:06 +1000

    stumbling still. idk if this stuff is any use

---
+++ firebase/public/config.ts
@@ -0,0 +1,11 @@
+  //databaseURL: 'https://PROJECT_ID.firebaseio.com',
+  //measurementId: 'G-MEASUREMENT_ID',
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyCY70CZPWKxkOO5L5BRBrOmyH5eB4oKdF4",
+    authDomain: "qasmiq.firebaseapp.com",
+    projectId: "qasmiq",
+    storageBucket: "qasmiq.appspot.com",
+    messagingSenderId: "548352322126",
+    appId: "1:548352322126:web:075578ba126fe91e57711f"
+};
